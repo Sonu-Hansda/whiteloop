d32929e76ec113000b0a99fd700eaf4ccabd8157
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-02 16:43:30 +0530

    cloning alter template from framer

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <main className="text-[16px] py-4 px-20">
+     <Navbar/>
+     <Hero/>
+    </main>
+  )
+}
+
+export default App
