bf7d6277bba68239b3cf50ee06a9c3bdafe86c65
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-03 23:54:01 +0530

    added integration section

---
+++ src/components/Integration.tsx
@@ -0,0 +1,14 @@
+import { BiSolidLayer } from "react-icons/bi"
+
+function Integration() {
+  return (
+    <div className="bg-[#d8dfe5] p-8 rounded-2xl">
+        <button className="flex items-center justify-center bg-white rounded-2xl px-4 py-2 border border-gray-300 gap-x-1.5">
+            <BiSolidLayer/>
+            <span className="text-sm">Integration</span>
+        </button>
+    </div>
+  )
+}
+
+export default Integration
