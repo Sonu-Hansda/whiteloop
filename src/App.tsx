d9cb7d1ab84b9671261861d8d588e6ce1769963e
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-03 23:54:38 +0530

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Hero from "./components/Hero"
+import Integration from "./components/Integration"
 import Navbar from "./components/Navbar"
+import Partners from "./components/Partners"
 
 function App() {
 
@@ -7,6 +9,8 @@ function App() {
     <main className="text-[16px] py-4 px-20">
      <Navbar/>
      <Hero/>
+     <Partners/>
+     <Integration/>
     </main>
   )
 }
