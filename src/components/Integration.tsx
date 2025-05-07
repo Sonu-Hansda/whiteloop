6bf3538a159a6cb8b46bc46fcc0232a178700ae8
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-07 18:59:27 +0530

    updated Integration section by updating font and their styles

--- src/components/Integration.tsx
+++ src/components/Integration.tsx
@@ -2,11 +2,15 @@ import { BiSolidLayer } from "react-icons/bi"
 
 function Integration() {
   return (
-    <div className="bg-[#d8dfe5] p-8 rounded-2xl">
-        <button className="flex items-center justify-center bg-white rounded-2xl px-4 py-2 border border-gray-300 gap-x-1.5">
+    <div className="bg-[#d8dfe5] p-8 rounded-2xl text-center mb-8">
+        <button className="flex mx-auto items-center justify-center bg-white rounded-2xl px-4 py-2 border border-gray-300 gap-x-1.5">
             <BiSolidLayer/>
             <span className="text-sm">Integration</span>
         </button>
+        <div>
+          <p className="text-5xl mb-3">Integrates with</p>
+          <p className="text-lg">Seamlessly integrate with your favorite tools</p>
+        </div>
     </div>
   )
 }
