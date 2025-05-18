aaa93293fd037fa5ad04d339fe5f4e76b3be629f
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-18 12:49:32 +0530

    updated background color

--- src/components/Chips.tsx
+++ src/components/Chips.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 function Chips(data:Props) {
   return (
-    <div className="bg-gray-400/35 py-2 px-4 rounded-full font-semibold">{data.text}</div>
+    <div className="bg-gray-400/15 py-2 px-4 rounded-full">{data.text}</div>
   )
 }
 
