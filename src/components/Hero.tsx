9cadcb3cc99a6df49c57876fa9d5c9b30542ea64
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-18 14:04:20 +0530

    added text responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,15 @@ import { TbHelpCircle } from "react-icons/tb"
 
 function Hero() {
     return (
-        <div className="my-12">
+        <div className="my-12 px-4">
             <div className="mx-auto p-2.5 rounded-lg shadow-lg w-24 border border-blue-800/10 drop-shadow-blue-800/75 drop-shadow-sm mb-6">
                 <img className="bg-[#0e1c29] p-2 rounded-sm" src="https://framerusercontent.com/images/Vw4SxE2qkEuKYpt0YYdU2kV6Irg.png" alt="Logo" />
             </div>
-            <p className="text-7xl text-center mb-4">
+            <p className="hidden md:block text-7xl text-center mb-4">
                 Automate. Engage. Convert. <br />Powered by AI.
+            </p>
+            <p className="block md:hidden text-4xl text-center mb-4">
+                Automate. Engage. <br/>Convert. Powered by AI.
             </p>
             <p className="text-center">Your journey to AI-powered marketing starts here</p>
             <div className="flex justify-center gap-x-12 mt-10 font-semibold">
