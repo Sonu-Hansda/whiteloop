3a86b51fdf87ba956fc7261ce1a59eac0411dfe7
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-18 14:04:37 +0530

    added grayscale to images

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,17 +1,17 @@
 function Partners() {
     return (
-        <div className="mb-12">
+        <div className="mb-12 px-4">
             <span className="flex items-center w-full mb-3">
-                <span className="flex-1 border-t border-dotted border-2 border-gray-300"></span>
-                <span className="shrink-0 px-20 text-gray-900">Adopted by renowned, trusted, and leading enterprises</span>
-                <span className="flex-1 border-t border-dotted border-2 border-gray-300"></span>
+                <span className="hidden md:flex-1 border-t border-dotted border-2 border-gray-300"></span>
+                <span className="shrink-0 md:px-20 mx-auto text-gray-900">Adopted by renowned, trusted, and leading enterprises</span>
+                <span className="hidden md:flex-1 border-t border-dotted border-2 border-gray-300"></span>
             </span>
-            <div className="flex items-center justify-center space-x-12">
-                <img className="object-contain scale-125 w-20 h-10" src="https://framerusercontent.com/images/YUxV3rqZ7Wvh1h97M9ho3OJO0.svg" alt="brand 1" />
-                <img className="object-contain scale-75 w-20 h-10" src="https://framerusercontent.com/images/EboQqPMe8PvYShxr0q82nHBVaec.svg" alt="brand 2" />
-                <img className="object-contain w-20 h-10" src="https://framerusercontent.com/images/bn15sRZOZ1zNW6U4lKel1VE9eA.svg" alt="brand 3" />
-                <img className="object-contain w-20 h-10" src="https://framerusercontent.com/images/iDUtqN0QXPf3CrA1JgKCl01lasg.svg" alt="brand 4" />
-                <img className="object-contain w-20 h-10" src="https://framerusercontent.com/images/vICkLbycO96hE8662eq2BfqKXs.svg" alt="brand 5" />
+            <div className="flex flex-wrap items-center justify-center space-x-12 mt-8 md:mt-auto">
+                <img className="object-contain scale-125 w-20 h-10 opacity-55" src="https://framerusercontent.com/images/YUxV3rqZ7Wvh1h97M9ho3OJO0.svg" alt="brand 1" />
+                <img className="object-contain scale-75 w-20 h-10 opacity-55" src="https://framerusercontent.com/images/EboQqPMe8PvYShxr0q82nHBVaec.svg" alt="brand 2" />
+                <img className="object-contain w-20 h-10 opacity-55" src="https://framerusercontent.com/images/bn15sRZOZ1zNW6U4lKel1VE9eA.svg" alt="brand 3" />
+                <img className="object-contain w-20 h-10 opacity-55" src="https://framerusercontent.com/images/iDUtqN0QXPf3CrA1JgKCl01lasg.svg" alt="brand 4" />
+                <img className="object-contain w-20 h-10 opacity-55" src="https://framerusercontent.com/images/vICkLbycO96hE8662eq2BfqKXs.svg" alt="brand 5" />
             </div>
         </div>
     )
