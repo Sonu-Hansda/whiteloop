95ccd91e03c273a2a89233001d4401f3d004054b
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-05-19 08:12:51 +0530

    added responsiveness for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,12 @@ import { PiInstagramLogo, PiLinkedinLogo, PiTwitterLogo, PiYoutubeLogo } from "r
 
 function Footer() {
     return (
-        <div>
-            <div className="flex justify-between mb-6">
-                <img className="h-8" src="https://framerusercontent.com/images/on7xUpJ1SM94eAeiyf04v5F4rjQ.png" />
-                <ul className="flex text-2xl space-x-2.5 text-gray-600">
+        <div className="px-8">
+            <div className="flex flex-col-reverse  md:flex-col border-b border-gray-400 mb-6">
+
+            <div className="flex flex-col md:flex-row md:justify-between mb-6">
+                <img className="h-24 md:h-8 w-24 md:w-auto object-contain mb-6" src="https://framerusercontent.com/images/on7xUpJ1SM94eAeiyf04v5F4rjQ.png" />
+                <ul className="flex text-2xl space-x-4 text-gray-600">
                     <li>
                         <PiInstagramLogo />
                     </li>
@@ -20,15 +22,16 @@ function Footer() {
                     </li>
                 </ul>
             </div>
-            <ul className="flex space-x-4 border-b border-gray-400 pb-8 mb-6 text-gray-600">
+            <ul className="flex flex-col md:flex-row space-x-4 space-y-4 pb-8 text-gray-600">
                 <li>Features</li>
                 <li>Changelog</li>
                 <li>Pricing</li>
                 <li>Contact</li>
             </ul>
-            <div className="text-sm flex justify-between">
-                <span>© whiteloop</span>
-                <span>Made by <a>Sonu-Hansda</a></span>
+            </div>
+            <div className="text-sm flex flex-col-reverse md:flex-row justify-between">
+                <span className="mb-4">© 2025 TheWhiteLoop</span>
+                <span className="mb-4">Made by <a className="underline">Sonu-Hansda</a></span>
             </div>
         </div>
     )
